630787908a805663a23c3e4fd9e520ba928c92eb
AAAF2A <2079230695@qq.com>
2025-09-23 16:00:00 +0800

    了

--- src/utilities/request.ts
+++ src/utilities/request.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request= axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://m1.apifoxmock.com/m1/7137920-6861064-default",
     timeout: 5000
 })
 
